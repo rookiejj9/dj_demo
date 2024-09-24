400404ea51c1696d469ff9269d2ecd2a9045810f
rookiejj9 <rookiejj9@gmail.com>
2024-09-24 18:01:17 +0800

    增加demo的url、views视图

--- demo/models.py
+++ demo/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class ProvinceList(models.Model):
+    """
+    城市表
+    """
+    Provinces = models.CharField(max_length=100)
+
+    def  __str__(self):
+        return self.Provinces
+
 class CityList(models.Model):
     """
     城市表
     """
-    city = models.CharField(max_length=100)
+    City = models.CharField(max_length=100)
+    Province = models.ForeignKey(ProvinceList,on_delete=models.CASCADE)
+
+    def  __str__(self):
+        return self.City
--- demo/views.py
+++ demo/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
+
+from django.views import generic
+from .models import ProvinceList,CityList
+
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'demo/index.html'
+    context_object_name = 'Provinces'
 
-def index(request):
-    return HttpResponse("this is index")
+    def get_queryset(self):
+        """
+        重写get_queryset，只返回省份名称
+        :return:
+        """
+        return ProvinceList.objects.order_by('id')
 
-
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Cities'] = CityList.objects.order_by('id')
+        return context
